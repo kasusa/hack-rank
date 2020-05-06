377a6e4be1dc217cfb86a4f2df47102119caa941
kasusa <kasusaland@gmail.com>
2020-05-06 17:40:27 +0800

    Create ponyphoto.bat

---
+++ py/ponywatcher/ponyphoto.bat
@@ -0,0 +1,9 @@
+REM how to use this :
+REM 1. install python on your windows computer
+REM 2. change cd directory to your py file 
+REM 3. make sure to install all python dependency first using : pip install "SomeProject"
+REM this line is for open a website using default browser explorer "https://www.derpibooru.org/"
+
+@ echo off
+cd C:\Users\kasus\Documents\git_project\hack-rank\py\ponywatcher
+python pwatch.py
