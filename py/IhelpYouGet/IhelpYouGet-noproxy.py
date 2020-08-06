c31b15e704f07bc3558e403708c5c2dbc467ea75
kasusa <kasusaland@gmail.com>
2020-08-06 15:40:35 +0800

    youget- noproxy

---
+++ py/IhelpYouGet/IhelpYouGet-noproxy.py
@@ -0,0 +1,21 @@
+import os
+
+if __name__ == '__main__':
+    line = "__________"
+    _http_proxy = "127.0.0.1:7890 "
+    _download_path = r"C:\Users\kasusa\Downloads"
+
+    to_say = f'''___ I help you get ___
+你可以在py代码顶部修改 download_path
+使用之前请确保已经安装了python和you-get
+you-get安装: pip3 install you-get
+{line}
+当前状态:
+download_path : {_download_path}
+{line}
+请粘贴要下载的视频链接:'''
+    print(to_say)
+    link = input("")
+    command = f"you-get -o {_download_path} {link}"
+    print("请耐心等待解析,马上就会显示下载进度...")
+    os.system(command)
